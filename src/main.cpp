f3fee572212396d731491447c4e39287cfb40d94
Caio Nunes <caioffnunes04@gmail.com>
2026-06-23 22:32:56 -0300

    Turning Vsync off

--- src/main.cpp
+++ src/main.cpp
@@ -151,6 +151,7 @@ int main(int argc, char* argv[])
     glfwSetScrollCallback(window, ScrollCallback);
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 
     glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
